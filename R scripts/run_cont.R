26b96e4926e4ca79efc8e91688cb207d05a0afb2
rahmarid <25745888+rahmarid@users.noreply.github.com>
2018-04-22 17:03:36 +0200

    Update and rename run4_400cont.R to run_cont.R

--- R scripts/run_cont.R
+++ R scripts/run_cont.R
@@ -6,17 +6,17 @@ registerDoParallel(c1)
 
 
 
-load("data4_400_cont.RData")
-numSubset <- 25
-num_iteration <- 30 
-all_Pop <- 50
-allMut <- 0.01
-allCross <- 0.45
-longi <- FALSE
-num_time <- 1
-the_co <- "covariance"
-cons_matrix <- NULL
-th <- 0.6
+load("data4_400_cont.RData") #to change the data
+numSubset <- 25 #to change the number of subset here
+num_iteration <- 30 # to change the number of iterations here
+all_Pop <- 50 #to change the number of models here
+allMut <- 0.01 #to change mutation rate
+allCross <- 0.45 #to change crossover rate
+longi <- FALSE # current version of S3C-Latent only supports cross-sectional data
+num_time <- 1 # set to 1
+the_co <- "covariance" #or "correlation"
+cons_matrix <- NULL #see documentation to set a constraint matrix
+th <- 0.6 
 to_plot <- FALSE
 mix <- FALSE
 imputed <- FALSE
@@ -27,7 +27,7 @@ toComplement <- FALSE
 
 result <- list()
 
-for (i in 1:10) {
+for (i in 1:20) {
   
   print(i)
   result[[i]] <- stablespecImptLatent::stableSpec(theData = data4_400[[i]], nSubset = numSubset,
