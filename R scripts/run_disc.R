8428157a0c646c4ea2d3d08838db12cf2a474373
rahmarid <25745888+rahmarid@users.noreply.github.com>
2018-05-28 00:31:34 +0700

    Update run_disc.R

--- R scripts/run_disc.R
+++ R scripts/run_disc.R
@@ -1,27 +1,24 @@
 library(parallel)
 library(doParallel)
-library(stablespecImptLatent)
-c2 <- makeCluster(30)
+library(s3clatent)
+c2 <- makeCluster(30) #set a cluster of 30 cores
 registerDoParallel(c2)
 
 
 
 load("data4_400_disc.RData") #to change the data
-numSubset <- 35
-num_iteration <- 30 
-all_Pop <- 50
-allMut <- 0.01
-allCross <- 0.45
-longi <- FALSE
-num_time <- 1
-the_co <- "covariance"
-cons_matrix <- NULL
-th <- 0.6
-to_plot <- FALSE
-mix <- TRUE
-imputed <- FALSE
-toMultiple <- FALSE
-toComplement <- FALSE
+numSubset <- 35 #to change the number of subsets
+num_iteration <- 30 #to change the number of iterations
+all_Pop <- 50 #to change the number of models in a population
+allMut <- 0.01 #to chagne the mutation rate/probability
+allCross <- 0.45 #to change the crossover rate/probability
+longi <- FALSE #set to FALSE as the current version of S3C-Latent only supports cross-sectional data
+num_time <- 1 #set to 1
+the_co <- "covariance" #doesn't matter for ordinal data.
+cons_matrix <- NULL #see documentation to set a constraint matrix
+th <- 0.6 #threshold of stability selection.
+mix <- TRUE #if ordinal data or a mixture of continuous and ordinal data.
+
 
 
 
@@ -34,10 +31,9 @@ for (i in 1:20) {
                                                   allIteration = num_iteration, allPop = all_Pop,
                                                   allMutRate = allMut, allCrossRate = allCross,
                                                   longitudinal = longi, numTime = num_time,
-                                                  co = "covariance", consMatrix = cons_matrix, threshold = th,
-                                                  toPlot = to_plot, mixture = mix, impute = imputed, 
-                                                  latentList = allLatent4[[i]],
-                                                  multiple = toMultiple, complementary = toComplement)
+                                                  co = "covariance", consMatrix = cons_matrix, 
+                                                  threshold = th, mixture = mix, 
+                                                  latentList = allLatent4[[i]])
   
 }
 stopCluster(c2)
